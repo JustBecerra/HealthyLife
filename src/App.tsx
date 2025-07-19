59df6bc96032c01f7774ff530dffbe390a85ef5f
justin <justj.becerra@gmail.com>
2025-07-19 14:32:26 -0300

    add prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-green-500 p-4">Vite + React</h1>
+      <h1 className="text-3xl font-bold underline text-green-500 p-4">
+        Vite + React
+      </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -19,7 +21,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
